1d5cb353344425cd3f5b760ab5ddaca23dc0d89a
mrlabel001 <farbean.makini@student.moringaschool.com>
2023-10-04 17:41:35 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+
+
+var cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
+function destructivelyAppendCat(){
+cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    }
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function appendCat(name){
+    var array = [...cats,"Broom"];
+    return array;
+}
+function prependCat(name){
+    var arrayTwo = ["Arnold",...cats];
+    return arrayTwo;
+  }
+  function removeLastCat(){
+    var arrayThree= cats.slice(0,cats.length-1);
+    return arrayThree;
+  }
+  function removeFirstCat(){
+    var arrayFour= cats.slice(1);
+    return arrayFour;
+  }
